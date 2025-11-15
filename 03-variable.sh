ae0aeacfc1ead5d30333158a35979ac77e7b4616
sreenath reddy <sreenath.chappidi89@gmail.com>
2025-11-15 16:27:11 +0530

    some messege

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine"
+echo "$PERSON1:: What are you doing now?"
+echo "$PERSON2:: I am learning Shell-Scripting $PERSON1"
+
