4ab0bfdccbf9ea2080648b4a0781359c3c2ea580
sreenath reddy <sreenath.chappidi89@gmail.com>
2025-11-15 16:10:36 +0530

    some messege

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,4 +3,4 @@
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh, I am fine"
 echo "Ramesh:: What are you doing now?"
-echo "I am learning Shell-Scripting Ramesh"
+echo "Suresh:: I am learning Shell-Scripting Ramesh"
