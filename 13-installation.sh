6fc81d45e6b844c986b485438f3a065ee4f1a054
sreenath reddy <sreenath.chappidi89@gmail.com>
2025-11-16 00:37:16 +0530

    variables

---
+++ 13-installation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root prevelage"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through atgs just like shell script
+    if [ $1 -ne 0 ]; then
+       echo "ERROR:: Installing $2 is failure"
+       exit 1
+    else
+    echo "Installing $2 is SUCCESS"
+    fi 
+}   
+
+
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongodb-mongosh"
